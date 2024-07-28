330b354cb8203f2bd621285de2b8e9ad97a36591
Mustafa Barodawala <24886321+itsjustmustafa@users.noreply.github.com>
2024-07-28 23:04:01 +1000

    Little more refactoring

--- render_cards.py
+++ render_cards.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 import json
+from typing import List
 
 vert_card_dim = Image.open("sorcerycardmagic.png").size
 
@@ -8,9 +9,19 @@ set_name = "trump set"
 
 set_dir = f"card_data/{set_name}"
 
+TEXT_LINE_HEIGHT = 20
+VERTICAL_CARD_TEXT_COORDS = [50, 497]
+MANA_COORDS = [51, 41]
+POWER_COORDS = [399, 41]
+NAME_COORDS = [225, 41]
+AVATAR_NAME_COORDS = [225, 40]
+AVATAR_LIFE_COORDS = [63, 40]
+AVATAR_ATTACK_COORDS = [387, 40]
+TYPELINE_TEXT_COORDS = [46, 470]
+
 
 def contrastText(draw, *args, **kwargs):
-    draw.text(*args, fill=(255, 255, 255), **kwargs, stroke_width=2)
+    draw.text(*args, fill=(255, 255, 255), **kwargs, stroke_width=3)
     draw.text(*args, fill=(0, 0, 0), **kwargs)
 
 
@@ -18,6 +29,44 @@ def bestFontSize(text_length):
     biggest_size = 20
     least_filled_length = 32
     return biggest_size * least_filled_length / max(text_length, least_filled_length)
+
+
+def getLinesFromText(text: str, max_line_length: int):
+    all_lines = []
+    for delimited_text in text.split("\n"):
+        delimited_text = delimited_text.strip()
+        words = delimited_text.split()
+        lines = [words[0]]
+        for word in words[1:]:
+            rendered_word = word
+            match word:
+                case "->":
+                    rendered_word = "→"
+                case "(1)":
+                    rendered_word = "⑴"
+                case "(2)":
+                    rendered_word = "⑵"
+                case "(3)":
+                    rendered_word = "⑶"
+                case "(4)":
+                    rendered_word = "⑷"
+                case "(5)":
+                    rendered_word = "⑸"
+                case "(6)":
+                    rendered_word = "⑹"
+                case "(7)":
+                    rendered_word = "⑺"
+                case "(8)":
+                    rendered_word = "⑻"
+                case "(9)":
+                    rendered_word = "⑼"
+
+            if len(lines[-1] + " " + rendered_word) <= max_line_length:
+                lines[-1] += " " + rendered_word
+            else:
+                lines.append(rendered_word)
+        all_lines.extend(lines)
+    return all_lines
 
 
 if __name__ == "__main__":
@@ -66,15 +115,30 @@ if __name__ == "__main__":
             anchor="mm",
         )
 
-        flavour_font = ImageFont.truetype(
-            "ComicMono.ttf", bestFontSize(len(avatar_card_data["flavour"]))
+        typeline_font = ImageFont.truetype(
+            "ComicMono.ttf", bestFontSize(len(avatar_card_data["typeline"]))
         )
         contrastText(
-            draw, [46, 470], avatar_card_data["flavour"], font=flavour_font, anchor="lm"
+            draw,
+            [46, 470],
+            avatar_card_data["typeline"],
+            font=typeline_font,
+            anchor="lm",
         )
 
-        for i, line in enumerate(avatar_card_data["text"]):
-            contrastText(draw, [50, 500 + 20 * i], line, font=bottom_font, anchor="lm")
+        max_line_length = 42
+        lines_of_text = getLinesFromText(avatar_card_data["text"], max_line_length)
+        for i, line in enumerate(lines_of_text):
+            contrastText(
+                draw,
+                [
+                    VERTICAL_CARD_TEXT_COORDS[0],
+                    VERTICAL_CARD_TEXT_COORDS[1] + TEXT_LINE_HEIGHT * i,
+                ],
+                line,
+                font=bottom_font,
+                anchor="lm",
+            )
 
         avatar_rendered.save(f"{set_dir}/rendered_cards/{avatar_card_data['name']}.png")
 
@@ -97,8 +161,19 @@ if __name__ == "__main__":
             font=top_font,
             anchor="mm",
         )
-        for i, line in enumerate(magic_card_data["text"]):
-            contrastText(draw, [50, 500 + 20 * i], line, font=bottom_font, anchor="lm")
+        max_line_length = 42
+        lines_of_text = getLinesFromText(magic_card_data["text"], max_line_length)
+        for i, line in enumerate(lines_of_text):
+            contrastText(
+                draw,
+                [
+                    VERTICAL_CARD_TEXT_COORDS[0],
+                    VERTICAL_CARD_TEXT_COORDS[1] + TEXT_LINE_HEIGHT * i,
+                ],
+                line,
+                font=bottom_font,
+                anchor="lm",
+            )
 
         card_rendered.save(f"{set_dir}/rendered_cards/{magic_card_data['name']}.png")
 
@@ -133,8 +208,18 @@ if __name__ == "__main__":
             font=top_font,
             anchor="mm",
         )
-        for i, line in enumerate(minion_card_data["text"]):
-            contrastText(draw, [50, 500 + 20 * i], line, font=bottom_font, anchor="lm")
+        max_line_length = 42
+        lines_of_text = getLinesFromText(minion_card_data["text"], max_line_length)
+        for i, line in enumerate(lines_of_text):
+            contrastText(
+                [
+                    VERTICAL_CARD_TEXT_COORDS[0],
+                    VERTICAL_CARD_TEXT_COORDS[1] + TEXT_LINE_HEIGHT * i,
+                ],
+                line,
+                font=bottom_font,
+                anchor="lm",
+            )
 
         card_rendered.save(f"{set_dir}/rendered_cards/{minion_card_data['name']}.png")
 
@@ -150,7 +235,9 @@ if __name__ == "__main__":
         contrastText(
             draw, [70, 340], site_card_data["name"], font=bottom_font, anchor="lm"
         )
-        for i, line in enumerate(site_card_data["text"]):
+        max_line_length = 60
+        lines_of_text = getLinesFromText(site_card_data["text"], max_line_length)
+        for i, line in enumerate(lines_of_text):
             contrastText(draw, [70, 367 + 20 * i], line, font=bottom_font, anchor="lm")
 
         card_rendered.save(f"{set_dir}/rendered_cards/{site_card_data['name']}.png")
